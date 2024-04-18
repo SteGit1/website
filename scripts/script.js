19eb066f7b7dd7d67dd8ee201d9f7eaa9b078870
SteGit1 <koselev17er@gmail.com>
2024-04-18 14:38:54 +0300

    проба js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,37 @@
-var counter = 0; 
+var l; 
 
-function onClickPhoto(element){
-    counter++;
-    element.innerHTML = counter;
+function onClickPhoto(element) {
+    var div = document.createElement("div");
+    document.body.appendChild(div);
+    
+    l = div.href;
+
+    div.style.position = "fixed";
+
+    div.style.width = "100%";
+    div.style.height = "100%";
+
+    div.style.top = 0;
+    div.style.bottom = 0;
+    div.style.right = 0;
+    div.style.left = 0;
+
+    div.style.background = "rgb(0, 0, 0, 0.5)";
+
+    addPhoto();
+    addInfo();
+}
+
+function addPhoto() {
+    var div = document.createElement("div");
+    document.body.appendChild(div);
+
+    div.style.position = "fixed";
+    div.style.top = "100px";
+    div.style.bottom = "100px";
+    div.style.left = "50px";
+}
+
+function addInfo() {
+
 }
